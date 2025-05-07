f3220c5f1d9896fe5cc0fa05fb2fa5a41e235612
AbdulWasayTabba <bscs2312144@szabist.pk>
2025-05-07 13:18:07 +0000

    retrive data from database and show in website through html

---
+++ HRWEB/JS/employee.js
@@ -0,0 +1,30 @@
+const Link ="https://reimagined-carnival-pj947jqj6rjx26xrp-5005.app.github.dev/employee"
+
+fetch(Link).then(response=>{
+    if(!response.ok){
+        throw new Error("Failed to Fetch Data");
+    }
+    return response.json();
+}).then(data=>{
+    const tbody = document.querySelector("#employeetable tbody");
+
+    data.forEach(e=>{
+        const row = document.createElement("tr");
+        row.innerHTML = `
+        <td>${e.employee_id}</td>
+        <td>${e.first_name}</td>
+        <td>${e.last_name}</td>
+        <td>${e.email}</td>
+        <td>${e.phone_number}</td>
+        <td>${e.hire_date}</td>
+        <td>${e.job_id}</td>
+        <td>${e.salary}</td>
+        <td>${e.commision_pct}</td>
+        <td>${e.manger_id}</td>
+        <td>${e.department_id}</td>
+        `;
+        tbody.appendChild(row);
+    })
+}).catch(err=>{
+    console.log(err.message);
+})
